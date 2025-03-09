cfa60678cb81f5fe71ea3a93287631af9692217a
kyles13 <kyles13@cs.washington.edu>
2025-03-08 21:42:55 -0800

    trying to get mouse look working

--- MoveComponent.gd
+++ MoveComponent.gd
@@ -4,16 +4,18 @@ class_name MoveComponent
 
 @export var _bodyToMove : CharacterBody3D = null
 
-@export var _moveAccel = 4
-@export var _maxSpeed = 25
+@export var _moveAccel = 2
+@export var _maxSpeed = 10
 var _drag = 0.0 # calcs in _ready
-@export var _jumpForce = 30
+@export var _jumpForce = 10
 @export var _gravity = 60
 
 var _pressedJump = false 
 var _moveVec : Vector3 # auto inits to Vector3.ZERO
 var _velocity : Vector3
 var _snapVec : Vector3 # how body "sticks" to the ground when moving
+var _mouseMotion : InputEventMouseMotion
+var _mouseSens # factor in mouse sensitivity 
 
 var frozen = false
 
@@ -23,15 +25,46 @@ func _ready():
 func init(_body_to_move : CharacterBody3D):
 	_bodyToMove = _body_to_move
 
-func set_move_vec(move_vec: Vector3):
-	_moveVec = move_vec.normalized()
+# 0: forward, 1: backward, 2: left, 3: right
+func set_move_vec(dir : int):
+	match dir:
+		0:
+			_moveVec += Vector3.FORWARD
+		1: 
+			_moveVec += Vector3.BACK
+		2:
+			_moveVec += Vector3.LEFT
+		3:
+			_moveVec += Vector3.RIGHT
+		_:
+			_moveVec = Vector3.ZERO
+	_moveVec = _moveVec.normalized()
+
+func set_mouse_motion(mm : InputEventMouseMotion, ms : float):
+	_mouseMotion = mm
+	_mouseSens = ms
+
+func clear_mouse_motion():
+	_mouseMotion = null
+
+func set_jump(pressedJump: bool):
+	_pressedJump = pressedJump
 
 func _physics_process(delta: float) -> void:
 	if frozen:
 		return
 	
+	# --- Mouse Motion ---
+	if _mouseMotion.relative.length() > 0.6:
+		print(_mouseMotion.relative.length())
+		_bodyToMove.rotation_degrees.y -= _mouseSens * _mouseMotion.relative.x
+		#print(_bodyToMove.rotation_degrees.y)
+		$Camera3D.rotation_degrees.x -= _mouseSens * _mouseMotion.relative.y
+		#print($Camera3D.rotation_degrees.x)
+		$Camera3D.rotation_degrees.x = clamp($Camera3D.rotation_degrees.x, -90, 90)
+
+	# --- Movement ---
 	var cur_move_vec = _moveVec
-	
 	_velocity += (_moveAccel * cur_move_vec) - (_velocity * Vector3(_drag, 0.0, _drag)) + (_gravity * Vector3.DOWN) * delta
 	_bodyToMove.set_velocity(_velocity)
 	_bodyToMove.move_and_slide()
@@ -42,6 +75,9 @@ func _physics_process(delta: float) -> void:
 	if grounded and _pressedJump:
 		_velocity.y = _jumpForce
 	_pressedJump = false
+
+
+
 
 func freeze():
 	frozen = true
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,17 +5,48 @@ class_name CorePlayer
 @export var _moveComponent : MoveComponent
 #signal input_info
 
+@export var _mouseSens = 0.5
+
+#func _ready():
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _process(delta: float) -> void:
 	var move_vec : Vector3
+	if _moveComponent == null:
+		_moveComponent.set_move_vec(-1)
+		return
+	
 	if (Input.is_action_pressed("move_forward")):
-		move_vec += Vector3.FORWARD
+		_moveComponent.set_move_vec(0)
 	if (Input.is_action_pressed("move_backward")):
-		move_vec += Vector3.BACK
+		_moveComponent.set_move_vec(1)
 	if (Input.is_action_pressed("move_left")):
-		move_vec += Vector3.LEFT
+		_moveComponent.set_move_vec(2)
 	if (Input.is_action_pressed("move_right")):
-		move_vec += Vector3.RIGHT
+		_moveComponent.set_move_vec(3)
+	if (Input.is_action_pressed("jump")):
+		_moveComponent.set_jump(true)
+	else:
+		_moveComponent.set_jump(false)
+	#if (Input.is_action_pressed("interact")):
+		# TODO: interacting, transfer control to rat
+		#pass
 	
 	#emit_signal("input_info", move_vec)
-	if _moveComponent != null:
-		_moveComponent.set_move_vec(move_vec)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_moveComponent.set_mouse_motion(event, _mouseSens)
+		#print(event.relative)
+	else:
+		_moveComponent.clear_mouse_motion()
+	#if event is InputEventKey and event.pressed:
+		#if event.scancode in hotkeys:
+			#weapon_manager.switch_to_weapon_slot(hotkeys[event.scancode])
+	#if event is InputEventMouseButton and event.pressed:
+		#if event.button_index == MOUSE_BUTTON_LEFT
+		#if event.button_index == BUTTON_WHEEL_DOWN:
+			#weapon_manager.switch_to_next_weapon()
+		#if event.button_index == BUTTON_WHEEL_UP:
+			#weapon_manager.switch_to_last_weapon()
+	
