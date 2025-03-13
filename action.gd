bcf053ae8bc26a6b3aefbf64b823c6450e567401
Kevin Lee <145483343+cowgoose@users.noreply.github.com>
2025-03-12 18:52:56 -0700

    stylizing introduction room

---
+++ action.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@export var dialogue_start: String= "start"
+@onready var spotlight = $"../SpotLight3D"
+@onready var roomlight = $"../roomlight"
+
+	
+
+func _on_timer_timeout():
+	$"../roomlight".visible = true
+	$"../SpotLight3D".visible = true
+	
+	await get_tree().create_timer(2).timeout
+	DialogueManager.show_dialogue_balloon(load("res://Dialogue/main.dialogue"), dialogue_start)
+	
